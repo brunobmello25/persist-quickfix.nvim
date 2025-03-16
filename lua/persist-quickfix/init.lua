c945b5493227708b559b7b840eb920fc71936e73
Bruno Mello <bruno.barros.mello@gmail.com>
2025-03-15 21:15:07 -0300

    add method to select between stored lists

--- lua/persist-quickfix/init.lua
+++ lua/persist-quickfix/init.lua
@@ -19,7 +19,10 @@ function M.save(name)
 	local json = vim.fn.json_encode(qflist)
 	local file, err = io.open(filepath, "w")
 	if not file then
-		vim.notify("Error opening file for writing quickfix: " .. err, vim.log.levels.ERROR)
+		vim.notify(
+			"Error opening file for writing quickfix: " .. err,
+			vim.log.levels.ERROR
+		)
 		return
 	end
 	file:write(json)
@@ -35,7 +38,10 @@ function M.load(name)
 
 	local file, err = io.open(filepath, "r")
 	if not file then
-		vim.notify("Error opening quickfix file for reading: " .. err, vim.log.levels.ERROR)
+		vim.notify(
+			"Error opening quickfix file for reading: " .. err,
+			vim.log.levels.ERROR
+		)
 		return
 	end
 
@@ -49,8 +55,26 @@ function M.load(name)
 		vim.cmd("copen")
 		vim.notify("Quickfix list loaded from " .. name, vim.log.levels.INFO)
 	else
-		vim.notify("Failed to decode quickfix list from JSON.", vim.log.levels.ERROR)
+		vim.notify(
+			"Failed to decode quickfix list from JSON.",
+			vim.log.levels.ERROR
+		)
 	end
+end
+
+function M.choose()
+	local ok, stored_lists = Utils.list_stored_lists(M.config.storage_dir)
+
+	if not ok then
+		return
+	end
+
+	vim.ui.select(stored_lists, {}, function(item)
+		if not item then
+			return
+		end
+		M.load(item)
+	end)
 end
 
 --- Setup persist-quickfix with user options.
--- lua/persist-quickfix/utils.lua
+++ lua/persist-quickfix/utils.lua
@@ -1,5 +1,26 @@
 local M = {}
 
+--- @param path string
+--- @return boolean, table
+function M.list_stored_lists(path)
+	local items = {}
+	local fd = vim.loop.fs_scandir(path)
+	if not fd then
+		vim.notify("Could not open directory: " .. path, vim.log.levels.ERROR)
+		return false, {}
+	end
+
+	while true do
+		local name, _ = vim.loop.fs_scandir_next(fd)
+		if not name then
+			break
+		end
+		table.insert(items, name)
+	end
+
+	return true, items
+end
+
 function M.convert_bfnumbers_to_paths(qflist)
 	for _, entry in ipairs(qflist) do
 		local bufnr = entry.bufnr
